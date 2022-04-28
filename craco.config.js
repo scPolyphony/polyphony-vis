cacf62e2bd9000e48501a4ddbd023d898168f070
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-04-28 12:39:21 -0400

    Update craco config

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,30 @@
+// const path = require('path');
+
 module.exports = {
     webpack: {
-        configure: {
-            resolve: {
+        configure: (webpackConfig, { env, paths }) => {
+            webpackConfig.resolve = {
+                ...webpackConfig.resolve,
                 alias: {
-                  'txml/txml': 'txml/dist/txml'
+                    'txml/txml': 'txml/dist/txml',
+                    ...webpackConfig.resolve.alias,
                 },
-            },
+            };
+            /*
+            webpackConfig.module.rules[2].oneOf[1].options.plugins = [
+                [
+                    path.join(paths.appNodeModules, 'babel-plugin-named-asset-import'),
+                    {
+                        loaderMap: {
+                            svg: {
+                                ReactComponent: '@svgr/webpack?-svgo,+titleProp,+ref![path]',
+                            },
+                        },
+                    },
+                ],
+            ];
+            */
+            return webpackConfig;
         },
     },
 };
